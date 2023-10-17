2e8a6b1cb161c90d51db5892b230f0802609bbf8
VincentRssx <Vincent.rssx59@gmail.com>
2023-10-17 11:06:28 +0200

    ajout mouseover

--- src/Composant/Body.jsx
+++ src/Composant/Body.jsx
@@ -6,22 +6,22 @@ function Body() {
 const animalArray = [
      
     [
-        { name: "Panpan", imgSrc: "https://images.pexels.com/photos/4588065/pexels-photo-4588065.jpeg?auto=compress&cs=tinysrgb&w=600" },
-        { name: "Punki", imgSrc: "https://images.pexels.com/photos/692071/pexels-photo-692071.jpeg?auto=compress&cs=tinysrgb&w=600" },
-        { name: "Civet", imgSrc: "https://images.pexels.com/photos/326012/pexels-photo-326012.jpeg?auto=compress&cs=tinysrgb&w=600" },
-        { name: "Civet", imgSrc: "https://cdn.pixabay.com/photo/2018/05/11/08/11/dog-3389729_640.jpg" },
+        { name: "Panpan", imgSrc: "https://images.pexels.com/photos/4588065/pexels-photo-4588065.jpeg?auto=compress&cs=tinysrgb&w=600", text:"choice me", },
+        { name: "Punki", imgSrc: "https://images.pexels.com/photos/692071/pexels-photo-692071.jpeg?auto=compress&cs=tinysrgb&w=600", text:"No, choice me !" },
+        { name: "Civet", imgSrc: "https://images.pexels.com/photos/326012/pexels-photo-326012.jpeg?auto=compress&cs=tinysrgb&w=600", text:"Realy, I'm better" },
+        { name: "Civet", imgSrc: "https://cdn.pixabay.com/photo/2018/05/11/08/11/dog-3389729_640.jpg", text:"I'm just a perfect dog" },
     ],
     
     [
-        { name: "Tajine", imgSrc: "https://images.pexels.com/photos/1317844/pexels-photo-1317844.jpeg?auto=compress&cs=tinysrgb&w=600" },
-        { name: "Moustache", imgSrc: "https://images.pexels.com/photos/2173872/pexels-photo-2173872.jpeg?auto=compress&cs=tinysrgb&w=600" },
-        { name: "Silver", imgSrc: "https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&w=600" },
+        { name: "Tajine", imgSrc: "https://images.pexels.com/photos/1317844/pexels-photo-1317844.jpeg?auto=compress&cs=tinysrgb&w=600",text:"I'm a cut cat" },
+        { name: "Moustache", imgSrc: "https://images.pexels.com/photos/2173872/pexels-photo-2173872.jpeg?auto=compress&cs=tinysrgb&w=600",text:"red Cat" },
+        { name: "Silver", imgSrc: "https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&w=600",text:"No, Choice me, serious cat"},
     ],
     
     [
-        { name: "Ruby", imgSrc: "https://images.pexels.com/photos/2607544/pexels-photo-2607544.jpeg?auto=compress&cs=tinysrgb&w=600" },
-        { name: "Diez", imgSrc: "https://images.pexels.com/photos/895259/pexels-photo-895259.jpeg?auto=compress&cs=tinysrgb&w=600" },
-        { name: "Voyou", imgSrc: "https://images.pexels.com/photos/4587971/pexels-photo-4587971.jpeg?auto=compress&cs=tinysrgb&w=600" },
+        { name: "Ruby", imgSrc: "https://images.pexels.com/photos/2607544/pexels-photo-2607544.jpeg?auto=compress&cs=tinysrgb&w=600",text:"take me, white dog"},
+        { name: "Diez", imgSrc: "https://images.pexels.com/photos/895259/pexels-photo-895259.jpeg?auto=compress&cs=tinysrgb&w=600",text:"No, Brown is better" },
+        { name: "Voyou", imgSrc: "https://images.pexels.com/photos/4587971/pexels-photo-4587971.jpeg?auto=compress&cs=tinysrgb&w=600",text:"No !, please Help me" },
     ]
 ]
 
